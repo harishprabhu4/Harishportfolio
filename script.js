a099365110d155bdfa06ae43b80c66d315e8a1cd
nivedha <nivedharaju11@gmail.com>
2021-08-12 23:15:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+      $(this).toggleClass('fa-times');
+      $('header').toggleClass('toggle');
+    });
+  
+    $(window).on('scroll load',function(){
+  
+      $('#menu').removeClass('fa-times');
+      $('header').removeClass('toggle');
+  
+      if($(window).scrollTop() > 0){
+        $('.top').show();
+      }else{
+        $('.top').hide();
+      }
+  
+    });
+
+    var typed = new Typed(".typing",{
+        strings:["UG-Bachelor of Engineering", "PG-Diploma in Advanced Computing"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    });
+  
+    // smooth scrolling 
+  
+    $('a[href*="#"]').on('click',function(e){
+  
+      e.preventDefault();
+  
+      $('html, body').animate({
+  
+        scrollTop : $($(this).attr('href')).offset().top,
+  
+      },
+        500, 
+        'linear'
+      );
+  
+    });
+  
+  });
